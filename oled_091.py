1cdb96126e9e3b6ebd9585dcda13b6641946de07
DieterUcll <43341550+DieterUcll@users.noreply.github.com>
2020-10-20 14:30:31 +0200

    Update oled_091.py
    " ' " character at o'clock gives encoding error

--- oled_091.py
+++ oled_091.py
@@ -270,7 +270,7 @@ class SSD1306(i2c_interface):
         Draws an arc (a portion of a circle outline) between the start and
         end angles, inside the given bounding box
         :param end: Starting angle, in degrees. Angles are measured from 3
-        o’clock, increasing clockwise.
+        o clock, increasing clockwise.
         :param start: Ending angle, in degrees.
         :param cords: Four points to define the bounding box
         :return: None
